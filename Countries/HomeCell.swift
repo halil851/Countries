796a816e32a07801303c9e23f63b9a418f937aec
halil851 <halil851@gmail.com>
2022-10-06 11:58:28 +0300

    save button gets fill

--- Countries/HomeCell.swift
+++ Countries/HomeCell.swift
@@ -10,10 +10,14 @@ import UIKit
 class HomeCell: UITableViewCell, UINavigationControllerDelegate {
     
     
+    @IBOutlet weak var savedButton: UIButton!
     
+  
     
     @IBOutlet weak var countryView: UIView!
     @IBOutlet weak var countryLabel: UILabel!
+    
+    var isSaved : Bool = false
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,5 +26,43 @@ class HomeCell: UITableViewCell, UINavigationControllerDelegate {
         
         
         
+        
     }
+    
+    
+    
+    @IBAction func savedButtonPressed(_ sender: Any) {
+        print("savedButtonPressed")
+        
+        
+        isSaved = !isSaved
+            if isSaved {
+                savedButton.setImage(UIImage(named: "star.fill.png"), for: .normal)
+            } else {
+                savedButton.setImage(UIImage(named: "star.png"), for: .normal)
+            }
+        
+//        let image = UIImage(named: "star.png")
+//        let imageFilled = UIImage(named: "star.fill.png")
+//        savedButton.setImage(image, for: .normal)
+//        savedButton.setImage(imageFilled, for: .selected)
+//
+//        savedButton.setImage(imageFilled, for: .selected)
+        
+        
+        //savedButton.setImage(UIImage(named: "star.fill.png"), for: .normal)
+//
+//        let starImage = UIImage(named: "star.fill.png")
+//        let starFillImage = UIImage(named: "star.png")
+//
+//        if savedButton.setImage == "star.fill.png" {
+//
+//        }
+//        savedButton.setImage(starImage, for: .normal)
+     
+        
+        
+        
+    }
+    
 }
